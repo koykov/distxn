4c9d84160af16aa6aa9f70b0cb7245da449061a5
Konstantin Voykov <koykov0@gmail.com>
2025-01-03 23:33:24 +0300

    2pc: init issue

--- 2pc/2pc.go
+++ 2pc/2pc.go
@@ -2,6 +2,9 @@ package twophasecommit
 
 import (
 	"context"
+	"math"
+	"sync"
+	"sync/atomic"
 
 	"github.com/koykov/distnx"
 )
@@ -25,14 +28,45 @@ func NewWithJobs(jobs ...distnx.Job) *TPC {
 
 func (dxn *TPC) Execute(ctx context.Context) error {
 	jobs := dxn.Jobs.Jobs()
+	n := len(jobs)
+	if cap(dxn.buf) < n {
+		dxn.buf = make([]distnx.Txn, n)
+	}
+	dxn.buf = dxn.buf[:n:n]
+
+	// Phase #1: prepare
+	var (
+		wg   sync.WaitGroup
+		errc        = make(chan error)
+		done        = make(chan struct{})
+		idx  uint32 = math.MaxUint32
+	)
+	wg.Add(n)
 	for i := 0; i < len(jobs); i++ {
-		txn, err := jobs[i].Begin(ctx)
-		if err != nil {
-			return err
-		}
-		dxn.buf = append(dxn.buf, txn)
+		go func(ctx context.Context, job distnx.Job, errc chan error) {
+			defer wg.Done()
+			txn, err := job.Begin(ctx)
+			if err != nil {
+				errc <- err
+				return
+			}
+			dxn.buf[atomic.AddUint32(&idx, 1)] = txn
+		}(ctx, jobs[i], errc)
+	}
+	go func(wg *sync.WaitGroup) {
+		wg.Wait()
+		close(done)
+	}(&wg)
+	select {
+	case err := <-errc:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-done:
+		// do nothing
 	}
 
+	// Phase #2: commit
 	for i := 0; i < len(dxn.buf); i++ {
 		if err := dxn.buf[i].Commit(ctx); err != nil {
 			for j := i; j >= 0; j-- {
